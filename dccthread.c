21d0406adc0b65f432275a931604d0320bd9505f
Bruno Wilson <brunowilson4@gmail.com>
2023-04-18 22:04:35 -0300

    Finaliza execução das threads no dccthread_init

--- dccthread.c
+++ dccthread.c
@@ -29,20 +29,40 @@ void dccthread_init(void (*func)(int), int param) {
     manager_thread = (dccthread_t*) malloc(sizeof(dccthread_t));
     getcontext(&manager_thread->state);
     strcpy(manager_thread->name, "manager");
+
+    manager_thread->state.uc_link = NULL;
+    manager_thread->state.uc_stack.ss_sp = malloc(THREAD_STACK_SIZE);
+	manager_thread->state.uc_stack.ss_size = THREAD_STACK_SIZE;
+	manager_thread->state.uc_stack.ss_flags = 0;
     
     // Inicializa thread principal
     main_thread = dccthread_create("main", func, param);
+
+    // Executa threads prontas para serem executadas
+    while (!dlist_empty(ready_threads_list))
+    {
+		swapcontext(&manager_thread->state, &((dccthread_t*) ready_threads_list->head->data)->state);
+		dlist_pop_left(ready_threads_list);
+    }
+
+    exit(EXIT_SUCCESS);
 }
 
 dccthread_t* dccthread_create(const char *name, void (*func)(int), int param) {
     // Inicializa uma nova thread
-    dccthread_t* new_thread = (dccthread_t*) malloc(sizeof(dccthread_t));
+    dccthread_t* new_thread; 
+    new_thread = (dccthread_t*) malloc(sizeof(dccthread_t));
     getcontext(&new_thread->state);
     strcpy(new_thread->name, name);
 
-    dlist_push_right(ready_threads_list, new_thread);
+    new_thread->state.uc_link = &manager_thread->state;
+    new_thread->state.uc_stack.ss_sp = malloc(THREAD_STACK_SIZE);
+	new_thread->state.uc_stack.ss_size = THREAD_STACK_SIZE;
+	new_thread->state.uc_stack.ss_flags = 0;
 
+    dlist_push_right(ready_threads_list, new_thread);
 	makecontext(&new_thread->state, (void*) func, 1, param);
+
     return new_thread;
 }
 
@@ -69,5 +89,5 @@ void dccthread_wait(dccthread_t *tid) {
 }
 
 void dccthread_sleep(struct timespec ts) {
-    
+
 }
