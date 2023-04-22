516f374166e45f19e50a6033b742f1528a253969
Bruno Wilson <brunowilson4@gmail.com>
2023-04-22 16:29:11 -0300

    refatora funções wait e exit, muda gerenciamento da thread manager

--- dccthread.c
+++ dccthread.c
@@ -9,26 +9,38 @@
 #define TRUE 1
 #define FALSE 0
 
-typedef unsigned char uint8_t;
+typedef unsigned char bool;
 
 typedef struct dccthread
 {
-    ucontext_t state;
+    ucontext_t context;
     char name[DCCTHREAD_MAX_NAME_SIZE];
-    uint8_t is_yielded;
-    
-    dccthread_t* queued_thread;
-    uint8_t is_ready;
-    uint8_t is_waiting;
+    dccthread_t* waiting_thread;
 } dccthread_t;
 
 // Threads gerente e principal
-dccthread_t* manager_thread;
+ucontext_t manager_thread_context;
 dccthread_t* main_thread;
 
 // Lista de threads prontas a serem executadas
 struct dlist* ready_threads_list;
 struct dlist* waiting_threads_list;
+
+bool find_thread_in_list(struct dlist* thread_list, dccthread_t* thread) {
+    if (thread_list->count == 0) return FALSE;
+
+    struct dnode* current_thread_item = thread_list->head;
+    while (current_thread_item != NULL && (dccthread_t*)current_thread_item->data != thread)
+    {
+        current_thread_item = current_thread_item->next;
+    }
+    
+    if (current_thread_item != NULL) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
 
 void dccthread_init(void (*func)(int), int param) {
     // inicializa listas de threads 
@@ -36,28 +48,29 @@ void dccthread_init(void (*func)(int), int param) {
     waiting_threads_list = dlist_create();
 
     // Inicializa thread gerente
-    manager_thread = (dccthread_t*) malloc(sizeof(dccthread_t));
-    getcontext(&manager_thread->state);
-    strcpy(manager_thread->name, "manager");
-    manager_thread->is_yielded = FALSE;
-    manager_thread->queued_thread = NULL;
-    manager_thread->state.uc_link = NULL;
-    manager_thread->state.uc_stack.ss_sp = malloc(THREAD_STACK_SIZE);
-	manager_thread->state.uc_stack.ss_size = THREAD_STACK_SIZE;
-	manager_thread->state.uc_stack.ss_flags = 0;
+    getcontext(&manager_thread_context);
     
     // Inicializa thread principal
     main_thread = dccthread_create("main", func, param);
 
     // Executa threads prontas para serem executadas
-    while (!dlist_empty(ready_threads_list))
+    while (!dlist_empty(ready_threads_list) || !dlist_empty(waiting_threads_list))
     {
         dccthread_t* current_ready_thread = (dccthread_t*) ready_threads_list->head->data;
-		swapcontext(&manager_thread->state, &current_ready_thread->state);
-        dlist_pop_left(ready_threads_list);
-        if (current_ready_thread->is_yielded) {
-            current_ready_thread->is_ready = FALSE;
+        dccthread_t* waiting_thread = current_ready_thread->waiting_thread;
+        
+        // verifica se está esperando por uma thread
+        if (waiting_thread != NULL) {
+            if (find_thread_in_list(ready_threads_list, waiting_thread)) {
+                dlist_push_right(ready_threads_list, current_ready_thread);
+                continue;
+            } else {
+                current_ready_thread->waiting_thread = NULL;
+            }
         }
+
+		swapcontext(&manager_thread_context, &current_ready_thread->context);
+        dlist_pop_left(ready_threads_list);
     }
 
     exit(EXIT_SUCCESS);
@@ -67,29 +80,27 @@ dccthread_t* dccthread_create(const char *name, void (*func)(int), int param) {
     // Inicializa uma nova thread
     dccthread_t* new_thread; 
     new_thread = (dccthread_t*) malloc(sizeof(dccthread_t));
-    getcontext(&new_thread->state);
+    getcontext(&new_thread->context);
     strcpy(new_thread->name, name);
-    new_thread->queued_thread = NULL;
+    new_thread->waiting_thread = NULL;
 
-    new_thread->state.uc_link = &manager_thread->state;
-    new_thread->state.uc_stack.ss_sp = malloc(THREAD_STACK_SIZE);
-	new_thread->state.uc_stack.ss_size = THREAD_STACK_SIZE;
-	new_thread->state.uc_stack.ss_flags = 0;
-    new_thread->is_ready = TRUE;
-    new_thread->is_waiting = FALSE;
-    new_thread->is_yielded = FALSE;
+    // aloca espaço para stack
+    new_thread->context.uc_link = &manager_thread_context;
+    new_thread->context.uc_stack.ss_sp = malloc(THREAD_STACK_SIZE);
+	new_thread->context.uc_stack.ss_size = THREAD_STACK_SIZE;
+	new_thread->context.uc_stack.ss_flags = 0;
 
+    // adiciona na lista de threads prontas e chama makecontext()
     dlist_push_right(ready_threads_list, new_thread);
-	makecontext(&new_thread->state, (void*) func, 1, param);
+	makecontext(&new_thread->context, (void*) func, 1, param);
 
     return new_thread;
 }
 
 void dccthread_yield(void) {
     dccthread_t* current_thread = dccthread_self();
-    current_thread->is_yielded = TRUE;
     dlist_push_right(ready_threads_list, current_thread);
-    swapcontext(&current_thread->state, &manager_thread->state);
+    swapcontext(&current_thread->context, &manager_thread_context);
 }
 
 dccthread_t* dccthread_self(void) {
@@ -100,46 +111,24 @@ const char* dccthread_name(dccthread_t* tid) {
     return tid->name;
 }
 
-int cmp_waiting_threads(const void *e1, const void *e2, void *userdata) {
-    dccthread_t* thread_item = (dccthread_t*) e1;
-    dccthread_t* thread_about_to_leave = (dccthread_t*) e2;
-    if (thread_about_to_leave == thread_item->queued_thread) {
-        return 0;
-    } else {
-        return 1;
-    }
-}
-
 void dccthread_exit(void) {
     // remover thread atual da lista de threads prontas
-    // Verificar se há uma thread aguardando sua finalização
-    // Se sim, Setar o contexto da thread a ela
-    // Se não, Setar o contexto da thread manager
+    // Setar o contexto da thread manager
 
     dccthread_t* current_thread = dccthread_self();
-    dccthread_t* queued_thread = (dccthread_t*) dlist_find_remove(waiting_threads_list, current_thread, cmp_waiting_threads, NULL);
-    if (queued_thread != NULL) {
-        queued_thread->is_ready = 1;
-        queued_thread->is_waiting = 0;
-        dlist_push_right(ready_threads_list, queued_thread);
-    }
-
-    setcontext(&manager_thread->state);
+    free(current_thread);
+    setcontext(&manager_thread_context);
 }
 
 void dccthread_wait(dccthread_t *tid) {
     // Pega a thread atual
-    // Seta a thread a esperar execução na variavel queued_thread
-    // Troca o contexto da thread sendo executada pela
+    // Seta a thread a esperar execução na variavel waiting_thread
+    // Troca o contexto da thread sendo executada pelo contexto da manager
 
     dccthread_t* current_thread = dccthread_self();
-    if (tid->is_ready || tid->is_waiting) {
-        current_thread->is_ready = 0;
-        current_thread->is_waiting = 1;
-        current_thread->queued_thread = tid;
-        dlist_push_right(waiting_threads_list, current_thread);
-        swapcontext(&current_thread->state, &manager_thread->state);
-    }
+    current_thread->waiting_thread = tid;
+    dlist_push_right(ready_threads_list, current_thread);
+    swapcontext(&current_thread->context, &manager_thread_context);
 }
 
 void dccthread_sleep(struct timespec ts) {
